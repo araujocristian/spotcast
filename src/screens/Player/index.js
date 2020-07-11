e6acd8c2e02c5b7556c5b4cea78839b1803785b1
Cristian Araujo <araujocristian@live.com>
2020-07-11 17:08:14 -0300

    Fazendo a interface do Player do Spotify E configurando LInt

--- src/screens/Player/index.js
+++ src/screens/Player/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { LinearGradient as Background } from 'expo-linear-gradient';
-import { TopBar } from './styles';
+import { TopBar, ScreenArea, CoverArea, PlayerArea, Controls, AudioSlider } from './styles';
 
 const PlayerScreen = () => {
+  const [seconds, setSeconds] = useState(0)
   return (
     <Background
       colors={['#464769', '#1B1A1F']}
@@ -14,7 +15,7 @@ const PlayerScreen = () => {
     >
       <TopBar>
         <TopBar.Left>
-          <MaterialCommunityIcons name="chevron-down" size={20} color="#fff"/>
+          <MaterialCommunityIcons name="chevron-down" size={20} color="#fff" />
         </TopBar.Left>
         <TopBar.Middle>
           <TopBar.Title>Tocando Podcast</TopBar.Title>
@@ -24,8 +25,50 @@ const PlayerScreen = () => {
           <MaterialIcons name="more-vert" size={20} color="#fff" />
         </TopBar.Right>
       </TopBar>
+      <ScreenArea>
+        <CoverArea>
+          <CoverArea.Image
+            resizeMode="contain"
+            source={{
+              uri: 'https://placehold.it/750x750',
+            }}
+          />
+        </CoverArea>
+
+        <PlayerArea>
+          <PlayerArea.Title>Angular vs React - Hipsters #142 </PlayerArea.Title>
+          <PlayerArea.Author>Hipsters Ponto Tech</PlayerArea.Author>
+          <Controls>
+            <Controls.Slider>
+              <AudioSlider
+                thumbTintColor="#ffffff"
+                minimumTrackTintColor="#1dd65f"
+                maximumTrackTintColor="#777777"
+                minimumValue={0}
+                maximumValue={100}
+                value={seconds}
+                onValueChange={(value) => setSeconds(value)}
+              />
+              <Controls.Slider.CurrentTime>
+                {seconds}
+              </Controls.Slider.CurrentTime>
+              <Controls.Slider.TotalTime>
+                52:07
+              </Controls.Slider.TotalTime>
+            </Controls.Slider>
+
+            <Controls.Play>
+              <MaterialCommunityIcons
+                name="play-circle"
+                size={106}
+                color="#fff"
+              />
+            </Controls.Play>
+          </Controls>
+        </PlayerArea>
+      </ScreenArea>
     </Background>
   );
-}
+};
 
 export default PlayerScreen;
--- src/screens/Player/styles.js
+++ src/screens/Player/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { Slider } from 'react-native';
 
 export const TopBar = styled.View`
   flex-direction: row;
@@ -28,4 +29,63 @@ TopBar.Title = styled.Text`
 TopBar.SubTitle = styled.Text`
   color: white;
   font-weight: bold;
+`;
+
+export const ScreenArea = styled.View`
+  flex: 1;
+  padding-left: 32px;
+  padding-right: 32px;
+  padding-bottom: 32px;
+`;
+
+export const CoverArea = styled.View`
+  flex: 4;
+`;
+
+CoverArea.Image = styled.Image`
+  width: 100%;
+  flex: 1;
+`;
+
+export const PlayerArea = styled.View`
+  flex: 2;
+  justify-content: flex-end;
+`;
+
+PlayerArea.Title = styled.Text`
+  color: white;
+  font-size: 20px;
+`;
+
+PlayerArea.Author = styled.Text`
+  color: #bbbbbb;
+  font-size: 16px;
+`;
+
+export const Controls = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+Controls.Play = styled.TouchableOpacity``;
+
+Controls.Slider = styled.View`
+  flex-basis: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+Controls.Slider.CurrentTime = styled.Text`
+  color: #bbbbbb;
+`;
+
+Controls.Slider.TotalTime = styled.Text`
+  color: #bbbbbb;
+`;
+
+export const AudioSlider = styled(Slider)`
+  flex-basis: 100%;
 `;
